613f6a803b4fbc6aad1d7484931fd69b24c7b346
Aleksandr Berdnikov <93481178+Flectrum@users.noreply.github.com>
2023-11-19 23:40:16 +0200

    Add files via upload

--- src/utils/ResultWriter.java
+++ src/utils/ResultWriter.java
@@ -6,11 +6,13 @@ import model.Player;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 public class ResultWriter {
     public static void writeResultToFile(List<Player> players, Casino casino) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("src/result.txt"));
+        players.sort(new PlayersComparator());
         for (Player player : players) {
             writer.write(player.getId() + " " + player.getBalance() + " " + player.getWinRate());
             writer.newLine();
